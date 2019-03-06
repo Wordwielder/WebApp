74c2c54359ff3e5e8079004a51ae53ac9c5f2c08
Wordwielder <wldltjng@gmail.com>
2019-03-06 14:21:06 +0900

    Update simplejquery.js

--- simplejquery.js
+++ simplejquery.js
@@ -54,5 +54,67 @@ $(document).ready(function() {
   // $("td:last-child").css("background","yellow");
 
   //$("td:nth-child(2n+1)").css("background","red");
-  $("td").filter(function(index){return index % 3 == 0}).css("background" ,"red");
+  //$("td").filter(function(index){return index % 3 == 0}).css("background" ,"red");
 });// 기본 필터, 폼에 적용하는 필터, 차일드에 적용하는 필터 3가지 유형이 있다
+
+//
+// $(document).ready(function() {
+//   $("td").not(function (index) {
+//     return index % 3 == 0; //일치하지 않는 것 선별
+//
+//
+//   }).css("background", "red"); // Traverse : 셀렉터에 의해 선별한 내용을 다시 한 번 선별//
+// });
+// //제이쿼리 문법은 셀렉터.메소드가 기본이다
+//
+// $(document).ready(function(){
+//   $("li").has("ul").css("background","red"); //해당하는 결과에 대해 그 내용이 포함되어 있는지 확인함
+// });
+
+$(document).ready(function(){
+  // $("*").each(function() {
+  //   if ($(this).is("li")){
+  //     $(this).css("border","1px solid red");
+  //   }
+  // });
+
+//   var litext = $("li").map(function(index)){
+//     return"[(" = index + ")" + $(this).text() +"]";
+//   }).get().join(',');
+//   $("#result").text(litext);
+ });
+//
+// function colorEm() {
+//   var $div = $("div");
+//   var start = Math.floor(Math.random() * $div.length);
+//   var end = Math.floor(Math.random() * ($div.length - start)) + start + 1;
+//
+//   if (end === $div.length) {end = undefined;}
+//   $div.css("background", "");
+//   if (end) {
+//     $div.slice(start,end).css("background", "yellow");
+//   } else {
+//     $div.slice(start).css("background", "yellow")
+//   }
+//   $("span").text("$(div).slice(" + start +
+//     (end ? ", " + end : "") + ").css('background,'yellow);");
+// }
+// $("button").click( colorEm );
+
+$(document).ready(function(){
+//  $("li").add("div > p").css("border", "1px solid red");
+  //$("ul").find("li").contents().remove("ul") //콘텐츠라는것은 해당 결과를 배열 형태로 리턴해줌
+  //end 이전 상태로 돌아감
+
+  //$("div").filter("P").css("border", "1px solid red");
+   // $("div").find("p").css("border", "1px solid red");
+   // $("div").filter(".my").css("border", "1px solid red");
+   // $("li").each(function(){
+   //   $(this).click(function(){
+   //     $(this).addClass("selected");
+   //     $(this).siblings().removeClass("selected");
+   //   });
+   // });
+   //$("td").css("background","blue")
+   $("td").css("background","blue")
+ });
